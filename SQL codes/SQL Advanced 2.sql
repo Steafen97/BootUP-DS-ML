e28efc7616b3422f65cf94634c73b5f876494961
Steafen97 <steafenk@gmail.com>
2020-08-02 17:12:46 +0700

    added phyton code to interact with MySQL,import and export data to MySQL

---
+++ SQL codes/SQL Advanced 2.sql
@@ -0,0 +1,30 @@
+show global variables like "local_infile";
+set global local_infile=1;
+show global variables like "local_infile";
+/**************************************************/
+use august;
+CREATE TABLE bank_customers (
+    rownumber INT,
+    customerid VARCHAR(20),
+    surname VARCHAR(40),
+    CreditScore INT,
+    Geography VARCHAR(20),
+    Gender VARCHAR(10),
+    Age INT,
+    Tenure INT,
+    Balance NUMERIC,
+    NumOfProducts INT,
+    HasCrCard INT,
+    IsActiveMember INT,
+    EstimatedSalary NUMERIC,
+    Exited INT
+);
+desc bank_customers;
+
+load data local infile "D:\\Data Steafen\\Coding\\Bank_churn_modelling.csv" into table bank_customers character set latin1 fields terminated by ',' enclosed by '"' lines terminated by "\r\n" ignore 1 lines;
+select * from bank_customers;
+select count(*) from bank_customers;
+
+use august;
+
+select * from bitcoin_data;
